67e91ebade3750da8cc997d7f365f3dcfd5158e1
Nishant Sahoo <nishantsahoo.in@gmail.com>
2019-01-13 19:14:35 +0530

    Update

--- App.js
+++ App.js
@@ -2,10 +2,29 @@ import React from 'react';
 import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  buttonClick() {
+  	Alert.alert(
+      'Yo',
+      "What's up?",
+      [
+        {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ],
+      { cancelable: false }
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Hello world!</Text>
+        <Button
+  			onPress={this.buttonClick.bind(this)}
+  			title="Learn More"
+  			color="#841584"
+  			accessibilityLabel="Learn more about this purple button"
+		/>
       </View>
     );
   }
